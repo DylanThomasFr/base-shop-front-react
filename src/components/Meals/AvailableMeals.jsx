78e4f9d932cd477d2993186c7c2d709d84f14a76
Dylan Toomas <thomas.dylan@fdvhuset.no>
2022-04-29 14:20:01 +0200

    Add fetching from db

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,37 +1,28 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import Card from '../UI/Card'
 import MealItem from './MealItem';
 
 import classes from './AvailableMeals.module.css'
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
 export default function AvailableMeals() {
-    const mealsList = DUMMY_MEALS.map(meal =>
+    const [meals, setMeals] = useState([])
+    useEffect(() => {
+        axios.get(process.env.VITE_APP_DATABASE_URL) 
+            .then(response => {
+                const loadedMeals = []
+                for (const key in response.data) {
+                    loadedMeals.push({
+                        id: key,
+                        ...response.data[key]
+                    })
+                }
+                setMeals(loadedMeals)
+            })
+    }, [])
+
+    const mealsList = meals.map(meal =>
         <MealItem
             key={meal.id}
             id={meal.id}
@@ -40,6 +31,7 @@ export default function AvailableMeals() {
             price={meal.price}
         />
     )
+
     return (
         <section className={classes.meals}>
             <Card>
